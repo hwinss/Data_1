01b0b1308f946121bcc4b6e192cb6abeb6223a22
hossein <you@example.com>
2023-01-13 22:27:33 +0330

    dash

---
+++ dash.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input,Outpot,State
+import plotly.graph_objects as go
+import plotly.express as px
+from dash import no_update
